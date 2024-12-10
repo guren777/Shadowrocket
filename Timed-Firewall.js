3814d16fc0f5286afa7b4c62ef63e685b0867e6e
guren777 <83865152+guren777@users.noreply.github.com>
2024-12-10 10:41:43 +0800

    更新 Timed-Firewall.js

--- Timed-Firewall.js
+++ Timed-Firewall.js
@@ -1,20 +1,20 @@
-const readTimer = $persistentStore.read("WiFi_Timer");
-const record = $persistentStore.read("Block_Num");
+const readTimer = $persistentStore.read("WiFi_Timer") || "0";
+const record = $persistentStore.read("Block_Num") || "0";
 let block = {
   matched: false
 };
 
 if (readTimer) {
   const currentTime = Date.now();
-  const markTime = parseInt(readTimer);
+  const markTime = parseInt(readTimer, 10); // 确保为数字
   if (currentTime - markTime <= 15000) {
     block.matched = true;
-    const addNum = JSON.stringify(parseInt(record || "0") + 1);
-    const saveNum = $persistentStore.write(addNum, "Block_Num");
+    const addNum = JSON.stringify(parseInt(record, 10) + 1);
+    $persistentStore.write(addNum, "Block_Num");
     console.log(`已拦截: ${$request.hostname}`);
   } else {
-    const delRecord = $persistentStore.write("", "Block_Num");
-    const delTime = $persistentStore.write("", "WiFi_Timer");
+    $persistentStore.write("", "Block_Num");
+    $persistentStore.write("", "WiFi_Timer");
     if (record) {
       console.log(`✅ 结束, 共 ${record} 条网络请求`);
     }
