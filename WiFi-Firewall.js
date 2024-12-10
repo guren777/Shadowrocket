2c9d37e29da8fab143640f79ca9ef5aa9386820f
guren777 <83865152+guren777@users.noreply.github.com>
2024-12-10 10:38:51 +0800

    更新 WiFi-Firewall.js

--- WiFi-Firewall.js
+++ WiFi-Firewall.js
@@ -1,21 +1,30 @@
 (async () => {
-	const network = $network.wifi.ssid;
-	if (network) {
-		const currentTime = Date.now();
-		const wifiEnv = $persistentStore.read("Firewall_Env");
-		if (!wifiEnv) { //if the script was last run in cellular env.
-			const startTime = await new Promise(res => {
-				$httpAPI("GET", "v1/traffic", null, (body) => res(body.startTime))
-			});
-			if ((currentTime / 1000) - startTime >= 3) {
-				const time = JSON.stringify(currentTime);
-				const addTime = $persistentStore.write(time, "WiFi_Timer");
-				const addEnv = $persistentStore.write("1", "Firewall_Env");
-				$notification.post('防火墙开始拦截', '', `已从蜂窝网络切换至 ${network}`);
-			}
-		}
-	} else {
-		const delEnv = $persistentStore.write("", "Firewall_Env");
-	}
-})().catch((err) => $notification.post('防火墙', '', `出现错误: ${err.message||err}`))
-	.finally(() => $done({}))
+  const network = $network.wifi.ssid;
+  const currentTime = Date.now();
+  
+  if (network) {
+    const wifiEnv = $persistentStore.read("Firewall_Env");
+    
+    if (!wifiEnv) { // 如果上次是在蜂窝环境运行
+      const lastSwitchTime = $persistentStore.read("WiFi_Timer");
+      
+      if (!lastSwitchTime || (currentTime - parseInt(lastSwitchTime)) >= 3000) { // 判断时间间隔是否 >= 3秒
+        const timeSaved = $persistentStore.write(currentTime.toString(), "WiFi_Timer");
+        const envSaved = $persistentStore.write("1", "Firewall_Env");
+        
+        if (timeSaved && envSaved) {
+          $notification.post("防火墙开始拦截", "", `已从蜂窝网络切换至 ${network}`);
+        } else {
+          $notification.post("防火墙", "", "存储数据时发生错误");
+        }
+      }
+    }
+  } else {
+    const envCleared = $persistentStore.write("", "Firewall_Env");
+    if (!envCleared) {
+      $notification.post("防火墙", "", "清除环境状态失败");
+    }
+  }
+})()
+  .catch((err) => $notification.post("防火墙", "", `出现错误: ${err.message || err}`))
+  .finally(() => $done({}));
