62f8c7b6dfe2df7ace57044859b51c9bd3d2ab3f
guren777 <83865152+guren777@users.noreply.github.com>
2025-04-09 22:58:21 +0800

    更新 bd_filter.js

--- bd_filter.js
+++ bd_filter.js
@@ -1,91 +1,30 @@
-var adAttr = '#content_left';
-var hintAttr = '.hint_common_restop';
-var blockRecord = 0;
-var resultSummaryAttr = '.head_nums_cont_inner .nums'
-var blockSummaryAttr = '.head_nums_cont_inner .nums #blockNum'
-var blockSummaryHtml = '，<a href="https://github.com/zhangyazhong/baidu-adblock" style="text-decoration:none;">屏蔽商业推广</a>已为您屏蔽<span id="blockNum">' + blockRecord + '</span>个推广广告</div>';
-var date = new Date();
-var today = 'day' + date.getFullYear() + date.getMonth() + date.getDate();
-var total = 'totalBlock';
-var showBlockSummary = 'showBlockSummary';
+// ==Script==
+// @name         百度搜索广告净化
+// @description  过滤百度搜索页中“广告”与“商业推广”模块
+// @match        https://www.baidu.com/s*
+// ==/Script==
 
-function isEmpty(obj) {
-    for (var property in obj){
-        return false;
-    }
-    return true;
+let body = $response.body;
+
+if (body) {
+  try {
+    // 过滤广告模块：带有“广告”、“商业推广”关键字的 DOM 节点
+    body = body.replace(/<div[^>]*class="[^"]*?result[^"]*?"[^>]*?>[\s\S]*?(广告|商业推广)[\s\S]*?<\/div>/g, '');
+
+    // 删除 hint 类提示
+    body = body.replace(/<div[^>]*class="hint_common_restop"[^>]*>[\s\S]*?<\/div>/g, '');
+
+    // 可选：替换广告提示为用户说明
+    body = body.replace(
+      /(<span[^>]*class="nums"[^>]*>.*?)(<\/span>)/,
+      '$1，已为您屏蔽百度广告<span style="color:red;">(已净化)</span>$2'
+    );
+
+    $done({ body });
+  } catch (e) {
+    console.log('百度净化失败:', e);
+    $done({});
+  }
+} else {
+  $done({});
 }
-
-function showBlockSummary() {
-    if ($(blockSummaryAttr) === null || $(blockSummaryAttr).length < 1) {
-        $(resultSummaryAttr).append(blockSummaryHtml);
-    }
-}
-
-function saveToHistory(blockCount) {
-    if (blockCount > 0) {
-        chrome.storage.local.get(total, function (result) {
-            if (result[total] === undefined) {
-                result[total] = 0;
-            }
-            result[total] += blockCount;
-            chrome.storage.local.set(result);
-        });
-        chrome.storage.local.get(today, function (result) {
-            if (result[today] === undefined) {
-                result[today] = 0;
-            }
-            result[today] += blockCount;
-            chrome.storage.local.set(result);
-        });
-    }
-}
-
-function block(dom) {
-    let tmpBlock = 0;
-    $.each(dom.children(), function(i, item) {
-        if ($(item).attr('id') === undefined || $(item).attr('id').length > 3) {
-            $(item).remove();
-            blockRecord++;
-            tmpBlock++;
-        } else if ($(item).find(".m").text().indexOf('广告') !== -1 || $(item).find(".m").text().indexOf('商业推广') !== -1) {
-            $(item).remove();
-            blockRecord++;
-            tmpBlock++;
-        }
-    });
-    if ($(blockSummaryAttr) !== null && $(blockSummaryAttr).length > 0) {
-        $(blockSummaryAttr).html(blockRecord);
-    }
-    window.setTimeout(function() {
-        saveToHistory(tmpBlock);
-    }, Math.floor(Math.random() * 1000));
-    dom.find(hintAttr).remove();
-}
-
-$('#wrapper').bind('DOMNodeInserted', function(e) {
-    if ($(e.target).find(adAttr).length > 0) {
-        block($(e.target).find(adAttr));
-    }
-});
-
-document.addEventListener('DOMNodeInserted', function() {
-    block($(adAttr));
-}, false);
-
-$(function() {
-    // chrome.storage.local.get(showBlockSummary, function (result) {
-    //     if (result[showBlockSummary] === undefined || result[showBlockSummary] > 0) {
-    //         var showBlockSummaryTimer = window.setInterval(function() {
-    //             if ($(blockSummaryAttr) === null || $(blockSummaryAttr).length < 1) {
-    //                 $(resultSummaryAttr).append(blockSummaryHtml);
-    //             }
-    //         }, 1000);
-    //     }
-    // });
-
-    // if (window.localStorage.getItem('showBlockSummary') === 'true') {
-    //     var showBlockSummaryTimer = window.setInterval("showBlockSummary()", 1000);
-    // }
-    block($(adAttr));
-});
